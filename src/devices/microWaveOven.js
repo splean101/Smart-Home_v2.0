4114c1c077130d08d1702a19e58725b66f9fcde6
splean101 <101splean@gmail.com>
2022-09-18 13:58:07 +0300

    added microwaveOven methods

--- src/devices/microWaveOven.js
+++ src/devices/microWaveOven.js
@@ -4,41 +4,46 @@ export class MicrowaveOven extends Device {
   constructor(model) {
     super(model);
     this._power = '0';
-    this._timer = false;
-    this._timerDelay = 10000;
     this._doorState = false;
+    //this._timer = false;
+    this._timerDelay = 10000;
+    this._interval = null;
     this._alarm = 'DING!';
   }
-  increasePower() {
-    // increasing power
-    if (!this._state) {
-      alert('Turn on the Microwave Oven');
-    } else if (this._power < 700) {
-      this._power = Number(this._power) + 100;
-    }
+  getPower() {
+    // getting power
+    return this._power;
   }
-  decreasePower() {
-    // decreasing power
-    if (this._power >= 100 && this._power <= 700 && this._state === false) {
-      alert('Turn on the Microwave Oven');
-    } else if (this._power > 0) {
-      this._power = Number(this._power) - 100;
-    }
+  setPower(power) {
+    // setting power
+    this._power = power;
   }
   openDoor() {
     //opening door
-    //........
+    return (this._doorState = true);
   }
   closeDoor() {
     // closing door
-    //........
+    return (this._doorState = false);
   }
-  urnOnTimer() {
+  turnOnTimer() {
     // turning timer on
-    //.............
+    if (_doorState) {
+      alert('Please, close the door');
+    } else {
+      _interval = setInterval(() => {
+        if (_timerDelay >= 0) {
+          _timerDelay -= 1000;
+          timeLeft.innerText = `Time left ${(_timerDelay + 1000) / 1000} s`;
+        } else {
+          clearInterval(_interval);
+          alert(_alarm);
+        }
+      }, 1000);
+    }
   }
   stopTimer() {
-    // turn timer off
-    //...........
+    // turning timer off
+    clearInterval(_interval);
   }
 }
