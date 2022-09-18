d27a6618ac83596aa0d95a538ee3d1ab8c9d1cb0
splean101 <101splean@gmail.com>
2022-09-18 12:46:10 +0300

    added airConditioner methods

--- src/devices/airConditioner.js
+++ src/devices/airConditioner.js
@@ -7,24 +7,24 @@ export class AirConditioner extends Device {
     this._currentTemperature = 20;
     this._turboMode = false;
   }
-  setMode() {
+  setMode(mode) {
     // set mode 'cooling' or 'heating'
-    //......
+    this._mode = mode;
   }
   getCurrentTemperature() {
     //getting current temperature
-    //.................
+    return this._currentTemperature;
   }
-  setTemperature() {
+  setTemperature(temperature) {
     //setting temperature
-    //.............
+    this._currentTemperature = temperature;
   }
   turboModeOn() {
     // turning mode turbo on
-    //...........
+    this._turboMode = true;
   }
   turboModeOff() {
     // turning mode turbo off
-    //...........
+    this._turboMode = false;
   }
 }
